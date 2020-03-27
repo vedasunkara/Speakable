36d6639e3110c19c357314514cf4acc605843022
Veda <veda_sunkara@brown.edu>
2020-03-26 18:29:07 -0700

    BackEnd
    BackEnd for Calculator Application.
    
    Functions() class handles explicit mathematical operations.
    
    Calculator() class stores state, handles parsing user inputs from frontend.

---
+++ Functions.java
@@ -0,0 +1,23 @@
+public class Functions {
+  public Functions() {}
+
+  public double add(double x, double y) {
+    return x+y;
+  }
+
+  public double subtract(double x, double y) {
+    return x-y;
+  }
+
+  public double multiply(double x, double y) {
+    return x*y;
+  }
+
+  public double divide(double x, double y) {
+    return x/y;
+  }
+
+  public double percentage(double x, double y) {
+    return (x/y) * 100;
+  }
+}
